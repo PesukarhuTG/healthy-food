739885b23b0ccd7b8bcb85b83ed8adb9a365fcf5
Tatiana <ganzhinovat@yandex.ru>
2020-10-22 19:23:12 +0300

    Create js timer

---
+++ js/scriptTimer.js
@@ -0,0 +1,57 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+const deadine = '2020-12-31';
+
+const getTimeRemaining = (endtime) => {
+    const t = Date.parse(endtime) - Date.parse(new Date()); //превращаем строку даты во что-то числовое
+    const days = Math.floor(t / (1000 * 60 * 60 * 24)); //ms в дни
+    const hours = Math.floor((t / (1000 * 60 * 60) % 24)); //ms в часы
+    const minutes = Math.floor((t / (1000 * 60)) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+        };
+    };
+
+//подставление нуля
+function getZero(num) {
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
+}
+
+const setClock = (selector, endtime) => {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock(); //чтобы не было мигания
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if (t.total <= 0 ) {
+            clearInterval(timeInterval);
+        }
+    }
+};
+
+setClock('.timer', deadine);
+
+});
