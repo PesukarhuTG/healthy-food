e36a7d80971d0378c1e4c1ac50463f46211b2e4d
Tatiana <ganzhinovat@yandex.ru>
2020-11-25 18:43:41 +0300

    Change: XMLHttpRequest ► Fetch

--- js/modalWindow.js
+++ js/modalWindow.js
@@ -80,10 +80,10 @@ const message = {
                 `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-            
+            // const request = new XMLHttpRequest();
+            // request.open('POST', 'server.php');
+            // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
             const formData = new FormData(form);
 
             const object = {};
@@ -91,21 +91,27 @@ const message = {
                 object[key] = value;
             });
 
-            request.send(JSON.stringify(object));
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                        console.log(request.response);
-                        showThanksModal(message.success);
-                        statusMessage.remove();
-                        form.reset();
-                    } else {
-                        showThanksModal(message.failure);
-                }
+                fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
+
         });
     }
-
 
     function showThanksModal(message) {
 
@@ -131,7 +137,6 @@ const message = {
             closeModal();
         }, 4000);
     }
-
 
 }
 
