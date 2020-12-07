ff3364fbca105c5e97ce1386ab19641593cb649b
Tatiana <ganzhinovat@yandex.ru>
2020-12-07 13:38:04 +0300

    Add axios library via cdn

--- js/goodsCard.js
+++ js/goodsCard.js
@@ -51,12 +51,20 @@ const generateGoodsCards = () => {
         return await result.json();
     };
 
-    getResource('http://localhost:3000/menu')
-        .then(data => {
-            data.forEach(({ img, altimg, title, descr, price }) => {
-                new MenuCard(img, altimg, title, descr, price).render();
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({ img, altimg, title, descr, price }) => {
+    //             new MenuCard(img, altimg, title, descr, price).render();
+    //         });
+    //     });
+
+    axios.get('http://localhost:3000/menu')
+            .then(data => {
+                data.data.forEach(({ img, altimg, title, descr, price }) => {
+                    new MenuCard(img, altimg, title, descr, price).render();
+                });
+                
             });
-        });
 }
 
 export default generateGoodsCards;
